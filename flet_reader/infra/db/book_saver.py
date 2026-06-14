c5bfba909c48e7ca67a0f42d85a01ffa908d35ad
milssky <2furius@gmail.com>
2026-06-14 16:07:52 +0300

    feat: book list component

--- flet_reader/infra/db/book_saver.py
+++ flet_reader/infra/db/book_saver.py
@@ -63,6 +63,8 @@ class BookSaver:
         chapters: list[Chapter],
     ) -> None:
         parent_ids: list[int] = []
+        # TODO: in some cases fb2 file doesn't contain some chapters
+        # need fallback to parse <p> blocks.
         for order_index, chapter in enumerate(chapters):
             self._validate_level(chapter.level, parent_ids)
             parent_ids = parent_ids[: chapter.level - 1]
